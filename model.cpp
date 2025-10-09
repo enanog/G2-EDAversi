e73249874665bfa3934ee181c71cc413bce66c95
Enzorofer <erosafernandez@itba.edu.ar>
2025-10-09 18:04:42 -0300

    New functions. Should now have functions for later additions

--- model.cpp
+++ model.cpp
@@ -1,386 +1,3 @@
-// /**
-//  * @brief Implements the Reversi game model
-//  * @author Marc S. Ressl
-//  * @modified:
-//  *			Agustin Valenzuela,
-//  *			Alex Petersen,
-//  *			Dylan Frigerio,
-//  *			Enzo Fernadez Rosas
-//  *
-//  *
-//  * @copyright Copyright (c) 2023-2024
-//  */
-
-// #include "model.h"
-
-// #include <stdint.h>
-
-// #include <bitset>
-// #include <iostream>
-
-// #include "raylib.h"
-
-// // Bitboard masks for board edges
-// #define FILE_A 0x0101010101010101ULL  // Left column	(x=0)
-// #define FILE_H 0x8080808080808080ULL  // Right column (x=7)
-// #define RANK_1 0x00000000000000FFULL  // Top row		(y=0)
-// #define RANK_8 0xFF00000000000000ULL  // Bottom row	(y=7)
-
-// #define DIRECTION_COUNT 8
-
-// namespace {
-
-// const int8_t DIRECTIONS[8] = {
-//     9,
-//     8,
-//     7,  // NW, N, NE
-//     1,
-//     /* O */ -1,  // W,    E
-//     -7,
-//     -8,
-//     -9  // SW, S, SE
-// };
-
-// // Initial four pieces setup in the board center
-// const int8_t initialPosition[2][2] = {{GET_SQUARE_BIT_INDEX(BOARD_SIZE / 2 - 1, BOARD_SIZE / 2),
-//                                        GET_SQUARE_BIT_INDEX(BOARD_SIZE / 2, BOARD_SIZE / 2 - 1)},
-//                                       {GET_SQUARE_BIT_INDEX(BOARD_SIZE / 2 - 1, BOARD_SIZE / 2 - 1),
-//                                        GET_SQUARE_BIT_INDEX(BOARD_SIZE / 2, BOARD_SIZE / 2)}};
-
-// /**
-//  * @brief Returns the index (0�63) of the least significant set bit in a bitboard.
-//  *
-//  * This is used to scan for the next occupied or empty square efficiently.
-//  */
-// inline int8_t bitScanForward(uint64_t bb) {
-// #if defined(_MSC_VER)  // MSVC (Windows)
-//     unsigned long idx;
-//     _BitScanForward64(&idx, bb);
-//     return (int8_t)idx;
-// #elif defined(__GNUC__) || defined(__clang__)  // GCC/Clang (Linux/macOS)
-//     return __builtin_ctzll(bb);
-// #else
-//     // Fallback
-//     int8_t idx = 0;
-//     while (((bb >> idx) & 1ULL) == 0)
-//         idx++;
-//     return idx;
-// #endif
-// }
-
-//     /**
-//      * @brief Converts a bit index (0�63) to a 2D board coordinate (Square_t).
-//      *
-//      * @param n Bit index.
-//      * @return Board coordinates (x, y).
-//      */
-//     inline Square_t transformBitToSquare(int8_t n) {
-//         Square_t position;
-//         position.x = n & 7;
-//         position.y = n >> 3;
-//         return position;
-//     }
-
-//     /**
-//      * @brief Calculates the discs that would be flipped in one direction.
-//      *
-//      * Iterates along the given direction until a player disc is found,
-//      * collecting opponent discs along the way. If the line is not closed,
-//      * no discs are flipped.
-//      *
-//      * @param player Bitboard of current player.
-//      * @param opponent Bitboard of opponent.
-//      * @param startPos Starting empty square index.
-//      * @param step Direction step offset.
-//      * @param dir Direction index.
-//      * @return Bitboard with flipped discs, or 0 if none.
-//      */
-//     uint64_t getFlipsInDirection(
-//         uint64_t player, uint64_t opponent, int8_t startPos, int step, int dir) {
-//         uint64_t flips = 0;
-//         int curPos = startPos + step;
-
-//         while (isSquareValid(curPos, dir)) {
-//             uint64_t curBit = 1ULL << curPos;
-
-//             if (opponent & curBit) {
-//                 flips |= curBit;
-//             } else if (player & curBit) {
-//                 return flips;
-//             } else {
-//                 break;
-//             }
-
-//             curPos += step;
-//         }
-
-//         return 0ULL;
-//     }
-
-//     /**
-//      * @brief Shift operations with edge masking
-//      * These ensure pieces don't "wrap around" board edges
-//      */
-//     inline uint64_t shiftN(uint64_t bb) {
-//         return bb >> 8;
-//     }  // UP (y decreases)
-//     inline uint64_t shiftS(uint64_t bb) {
-//         return bb << 8;
-//     }  // DOWN (y increases)
-//     inline uint64_t shiftE(uint64_t bb) {
-//         return (bb & ~FILE_H) << 1;
-//     }  // RIGHT (mask H, prevent wrap to A)
-//     inline uint64_t shiftW(uint64_t bb) {
-//         return (bb & ~FILE_A) >> 1;
-//     }  // LEFT (mask A, prevent wrap to H)
-//     inline uint64_t shiftNE(uint64_t bb) {
-//         return (bb & ~FILE_H) >> 7;
-//     }  // UP-RIGHT
-//     inline uint64_t shiftNW(uint64_t bb) {
-//         return (bb & ~FILE_A) >> 9;
-//     }  // UP-LEFT
-//     inline uint64_t shiftSE(uint64_t bb) {
-//         return (bb & ~FILE_H) << 9;
-//     }  // DOWN-RIGHT
-//     inline uint64_t shiftSW(uint64_t bb) {
-//         return (bb & ~FILE_A) << 7;
-//     }  // DOWN-LEFT
-
-//     /**
-//      * @brief Generates valid moves in one direction using kogge-stone propagation
-//      *
-//      * @param player Current player's pieces
-//      * @param opponent Opponent's pieces
-//      * @param shiftFunc Function pointer for directional shift
-//      * @return Bitboard of valid moves in this direction
-//      */
-//     uint64_t generateMovesInDirection(uint64_t player,
-//                                     uint64_t opponent,
-//                                     uint64_t (*shiftFunc)(uint64_t)) {
-//         uint64_t empty = ~(player | opponent);
-
-//         // Start with opponent pieces adjacent to player pieces
-//         uint64_t candidates = shiftFunc(player) & opponent;
-
-//         // Propagate through opponent pieces (kogge-stone algorithm)
-//         // Each iteration doubles the propagation distance
-//         candidates |= shiftFunc(candidates) & opponent;
-//         candidates |= shiftFunc(candidates) & opponent;
-//         candidates |= shiftFunc(candidates) & opponent;
-//         candidates |= shiftFunc(candidates) & opponent;
-//         candidates |= shiftFunc(candidates) & opponent;
-//         // 6 iterations = max propagation of 64 squares (2^6)
-
-//         // One final shift into empty squares gives us valid moves
-//         return shiftFunc(candidates) & empty;
-//     }
-
-//     /**
-//      * @brief Fast parallel move generation using kogge-stone algorithm.
-//      *
-//      * This is 5-10x faster than checking each square individually.
-//      * It uses bit-parallel operations to find all valid moves simultaneously.
-//      *
-//      * @param player Current player's bitboard
-//      * @param opponent Opponent's bitboard
-//      * @return Bitboard with all valid moves
-//      */
-//     uint64_t getValidMovesBitmap(uint64_t player, uint64_t opponent) {
-//         uint64_t legal = 0ULL;
-
-//         // Check all 8 directions
-//         legal |= generateMovesInDirection(player, opponent, shiftN);
-//         legal |= generateMovesInDirection(player, opponent, shiftS);
-//         legal |= generateMovesInDirection(player, opponent, shiftE);
-//         legal |= generateMovesInDirection(player, opponent, shiftW);
-//         legal |= generateMovesInDirection(player, opponent, shiftNE);
-//         legal |= generateMovesInDirection(player, opponent, shiftNW);
-//         legal |= generateMovesInDirection(player, opponent, shiftSE);
-//         legal |= generateMovesInDirection(player, opponent, shiftSW);
-
-//         return legal;
-//     }
-// }  // namespace
-
-// void initModel(GameModel& model) {
-//     model.gameOver = true;
-
-//     model.playerTime[0] = 0;
-//     model.playerTime[1] = 0;
-
-//     model.board.black = 0;
-//     model.board.white = 0;
-// }
-
-// void startModel(GameModel& model) {
-//     model.board.black = 0;
-//     model.board.white = 0;
-
-//     model.gameOver = false;
-
-//     model.currentPlayer = PLAYER_BLACK;
-
-//     model.playerTime[0] = 0;
-//     model.playerTime[1] = 0;
-//     model.turnTimer = GetTime();
-
-//     SET_BIT(model.board.black, initialPosition[0][0]);
-//     SET_BIT(model.board.black, initialPosition[0][1]);
-//     SET_BIT(model.board.white, initialPosition[1][0]);
-//     SET_BIT(model.board.white, initialPosition[1][1]);
-
-//     /*std::cout << "Black: " << std::bitset<64>(model.board.black) << std::endl;
-//     std::cout << "White: " << std::bitset<64>(model.board.white) << std::endl;*/
-// }
-
-// PlayerColor_t getCurrentPlayer(GameModel& model) {
-//     return model.currentPlayer;
-// }
-
-// int getScore(GameModel& model, PlayerColor_t player) {
-//     int score = 0;
-
-//     uint64_t bitmap = (player == PLAYER_BLACK) ? model.board.black : model.board.white;
-
-// #ifdef _MSC_VER
-//     return __popcnt64(bitmap);
-// #elif defined(__GNUC__) || defined(__clang__)
-//     return __builtin_popcountll(bitmap);
-// #else
-//     // Fallback: Brian Kernighan
-//     int count = 0;
-//     while (bitmap) {
-//         bitmap &= (bitmap - 1);
-//         count++;
-//     }
-//     return count;
-// #endif
-// }
-
-// double getTimer(GameModel& model, PlayerColor_t player) {
-//     double turnTime = 0;
-
-//     if (!model.gameOver && (player == model.currentPlayer))
-//         turnTime = GetTime() - model.turnTimer;
-
-//     return model.playerTime[player] + turnTime;
-// }
-
-// SquareState_t getBoardPiece(GameModel& model, int8_t n) {
-//     //	if (!isSquareValid(square))
-//     //		return PIECE_EMPTY;
-//     return (GET_BIT(model.board.black, n)
-//                 ? SQUARE_BLACK
-//                 : (GET_BIT(model.board.white, n) ? SQUARE_WHITE : SQUARE_EMPTY));
-// }
-
-// void setBoardPiece(GameModel& model, int8_t n, SquareState_t piece) {
-//     if (piece == SQUARE_BLACK) {
-//         SET_BIT(model.board.black, n);
-//         CLEAR_BIT(model.board.white, n);
-//     } else if (piece == SQUARE_WHITE) {
-//         SET_BIT(model.board.white, n);
-//         CLEAR_BIT(model.board.black, n);
-//     } else  // if(piece == SQUARE_EMPTY)
-//     {
-//         CLEAR_BIT(model.board.black, n);
-//         CLEAR_BIT(model.board.white, n);
-//     }
-// }
-
-// /**
-//  * @brief Checks whether a square index is valid for movement in a given direction.
-//  *
-//  * Prevents moves that would wrap around the board edges.
-//  *
-//  * @param pos Square index (0�63).
-//  * @param dir Direction index.
-//  * @return True if the square is valid for stepping, false otherwise.
-//  */
-// bool isSquareValid(int8_t pos, int dir) {
-//     if (pos < 0 || pos >= 64)
-//         return false;
-
-//     if (dir == NONE)
-//         return true;
-//     uint64_t bit = 1ULL << pos;
-
-//     if ((dir == W || dir == NW || dir == SW) && (bit & FILE_A))
-//         return false;
-//     if ((dir == E || dir == NE || dir == SE) && (bit & FILE_H))
-//         return false;
-//     if ((dir == N || dir == NW || dir == NE) && (bit & RANK_1))
-//         return false;
-//     if ((dir == S || dir == SW || dir == SE) && (bit & RANK_8))
-//         return false;
-
-//     return true;
-// }
-
-// void getValidMoves(GameModel& model, Moves& validMoves) {
-//     uint64_t player =
-//         (getCurrentPlayer(model) == PLAYER_BLACK) ? model.board.black : model.board.white;
-//     uint64_t opponent =
-//         (getCurrentPlayer(model) == PLAYER_BLACK) ? model.board.white : model.board.black;
-//     uint64_t validMovesBitmap = getValidMovesBitmap(player, opponent);  // Calculate once
-
-//     while (validMovesBitmap) {
-//         int8_t n = bitScanForward(validMovesBitmap);
-//         validMoves.push_back(transformBitToSquare(n));
-//         validMovesBitmap &= validMovesBitmap - 1;  // Clear LSB
-//     }
-// }
-
-// bool playMove(GameModel& model, int8_t move) {
-//     /// Get board state BEFORE placing piece
-//     PlayerColor_t currentPlayer = getCurrentPlayer(model);
-//     uint64_t player = (currentPlayer == PLAYER_BLACK) ? model.board.black : model.board.white;
-//     uint64_t opponent = (currentPlayer == PLAYER_BLACK) ? model.board.white : model.board.black;
-
-//     // Place the piece
-//     SquareState_t piece = (currentPlayer == PLAYER_BLACK) ? SQUARE_BLACK : SQUARE_WHITE;
-//     setBoardPiece(model, move, piece);
-
-//     // Flip discs in all directions
-//     for (int dir = 0; dir < DIRECTION_COUNT; dir++) {
-//         uint64_t flips = getFlipsInDirection(player, opponent, move, DIRECTIONS[dir], dir);
-//         if (flips) {
-//             if (currentPlayer == PLAYER_BLACK) {
-//                 model.board.black |= flips;
-//                 model.board.white &= ~flips;
-//             } else {
-//                 model.board.white |= flips;
-//                 model.board.black &= ~flips;
-//             }
-//         }
-//     }
-
-//     // Update timer
-//     double currentTime = GetTime();
-//     model.playerTime[model.currentPlayer] += currentTime - model.turnTimer;
-//     model.turnTimer = currentTime;
-
-//     // Swap player
-//     model.currentPlayer = (model.currentPlayer == PLAYER_WHITE) ? PLAYER_BLACK : PLAYER_WHITE;
-
-//     // Check if next player has valid moves
-//     Moves validMoves;
-//     getValidMoves(model, validMoves);
-
-//     if (validMoves.size() == 0) {
-//         // Swap player
-//         model.currentPlayer = (model.currentPlayer == PLAYER_WHITE) ? PLAYER_BLACK : PLAYER_WHITE;
-
-//         Moves validMoves;
-//         getValidMoves(model, validMoves);
-
-//         if (validMoves.size() == 0)
-//             model.gameOver = true;
-//     }
-
-//     return true;
-// }
-
 /**
  * @brief Implements the Reversi game model
  * @author Marc S. Ressl
@@ -660,6 +277,48 @@ void unmakeMove(Board_t& board, PlayerColor_t& currentPlayer, const BoardState_t
     board.black = state.black;
     board.white = state.white;
     currentPlayer = state.player;
+}
+
+int getMoveCount(const Board_t& board, PlayerColor_t player) {
+    uint64_t playerBB = (player == PLAYER_BLACK) ? board.black : board.white;
+    uint64_t opponentBB = (player == PLAYER_BLACK) ? board.white : board.black;
+    return countBits(getValidMovesBitmap(playerBB, opponentBB));
+}
+
+bool isMoveValid(const Board_t& board, PlayerColor_t player, Move_t move) {
+    if (move < 0 || move >= 64)
+        return false;
+
+    // Square must be empty
+    if (!isEmpty(board, move))
+        return false;
+
+    uint64_t playerBB = getPlayerBitboard(board, player);
+    uint64_t opponentBB = getOpponentBitboard(board, player);
+
+    // Check if this move would flip any pieces
+    return calculateFlips(playerBB, opponentBB, move) != 0ULL;
+}
+
+void printBoardDebug(const Board_t& board, PlayerColor_t currentPlayer) {
+    std::cout << "\n  a b c d e f g h\n";
+    for (int y = 0; y < 8; y++) {
+        std::cout << (y + 1) << " ";
+        for (int x = 0; x < 8; x++) {
+            int8_t n = GET_SQUARE_BIT_INDEX(x, y);
+            if (GET_BIT(board.black, n))
+                std::cout << "● ";  // Black disc
+            else if (GET_BIT(board.white, n))
+                std::cout << "○ ";  // White disc
+            else
+                std::cout << "· ";  // Empty
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "\nBlack: " << countBits(board.black) << " | White: " << countBits(board.white)
+              << " | Empty: " << getEmptyCount(board)
+              << " | Turn: " << (currentPlayer == PLAYER_BLACK ? "Black" : "White") << "\n\n";
 }
 
 // ============================================================================
--- model.h
+++ model.h
@@ -1,180 +1,3 @@
-// /**
-//  * @brief Implements the Reversi game model
-//  * @author Marc S. Ressl
-//  * @modified:
-//  *			Agustin Valenzuela,
-//  *			Alex Petersen,
-//  *			Dylan Frigerio,
-//  *			Enzo Fernadez Rosas
-//  *
-//  * @copyright Copyright (c) 2023-2024
-//  */
-
-// #ifndef MODEL_H
-// #define MODEL_H
-
-// #include <stdint.h>
-
-// #include <cstdint>
-// #include <vector>
-
-// #define BOARD_SIZE 8
-
-// // Board coordinate mapping:
-// // (0, 0) -> Top-Left
-// // (7, 7) -> Bottom-Right
-
-// /**
-//  * @brief Converts (x,y) coordinates into a bit index (0�63).
-//  */
-// #define GET_SQUARE_BIT_INDEX(x, y) ((x) + ((y) << 3))
-
-// /**
-//  * @brief Sets a bit in a bitboard.
-//  */
-// #define SET_BIT(bitmap, n) ((bitmap) |= (1ULL << (n)))
-
-// /**
-//  * @brief Retrieves a bit from a bitboard.
-//  */
-// #define GET_BIT(bitmap, n) (((bitmap) >> (n)) & 1ULL)
-
-// /**
-//  * @brief Clears a bit in a bitboard.
-//  */
-// #define CLEAR_BIT(bitmap, n) ((bitmap) &= ~(1ULL << (n)))
-
-// enum { NW, N, NE, W, E, SW, S, SE, NONE };
-
-// /**
-//  * @brief Represents a square position in 2D coordinates.
-//  */
-// typedef struct {
-//     int8_t x;
-//     int8_t y;
-// } Square_t;
-
-// #define GAME_INVALID_SQUARE {(int8_t)-1, (int8_t)-1}
-
-// /**
-//  * @brief State of a square on the board.
-//  */
-// typedef enum { SQUARE_BLACK, SQUARE_WHITE, SQUARE_EMPTY } SquareState_t;
-
-// /**
-//  * @brief Identifies player color (mapped to square states).
-//  */
-// typedef enum { PLAYER_BLACK = SQUARE_BLACK, PLAYER_WHITE = SQUARE_WHITE } PlayerColor_t;
-
-// /**
-//  * @brief Holds two bitboards (black and white pieces).
-//  */
-// typedef struct {
-//     uint64_t black;
-//     uint64_t white;
-// } Board_t;
-
-// /**
-//  * @brief Main game model storing board, players, and timers.
-//  */
-// struct GameModel {
-//     bool gameOver;
-
-//     double playerTime[2];
-//     double turnTimer;
-
-//     Board_t board;
-//     PlayerColor_t currentPlayer;
-//     PlayerColor_t humanPlayer;
-// };
-
-// typedef std::vector<Square_t> Moves;
-
-// /**
-//  * @brief Initializes a game model.
-//  *
-//  * @param model The game model.
-//  */
-// void initModel(GameModel& model);
-
-// /**
-//  * @brief Starts a game.
-//  *
-//  * @param model The game model.
-//  */
-// void startModel(GameModel& model);
-
-// /**
-//  * @brief Returns the model's current player.
-//  *
-//  * @param model The game model.
-//  * @return PLAYER_WHITE or PLAYER_BLACK.
-//  */
-// PlayerColor_t getCurrentPlayer(GameModel& model);
-
-// /**
-//  * @brief Returns the model's current score.
-//  *
-//  * @param model The game model.
-//  * @param player The player (PLAYER_WHITE or PLAYER_BLACK).
-//  * @return The score.
-//  */
-// int getScore(GameModel& model, PlayerColor_t player);
-
-// /**
-//  * @brief Returns the game timer for a player.
-//  *
-//  * @param model The game model.
-//  * @param player The player (PLAYER_WHITE or PLAYER_BLACK).
-//  * @return The time in seconds.
-//  */
-// double getTimer(GameModel& model, PlayerColor_t player);
-
-// /**
-//  * @brief Return a model's piece.
-//  *
-//  * @param model The game model.
-//  * @param square The square.
-//  * @return The piece at the square.
-//  */
-// SquareState_t getBoardPiece(GameModel& model, int8_t n);
-
-// /**
-//  * @brief Sets a model's piece.
-//  *
-//  * @param model The game model.
-//  * @param square The square.
-//  * @param piece The piece to be set
-//  */
-// void setBoardPiece(GameModel& model, int8_t n, SquareState_t piece);
-
-// /**
-//  * @brief Checks whether a square is within the board.
-//  *
-//  * @param square The square.
-//  * @return True or false.
-//  */
-// bool isSquareValid(int8_t pos, int dir);
-
-// /**
-//  * @brief Returns a list of valid moves for the current player.
-//  *
-//  * @param model The game model.
-//  * @param validMoves A list that receives the valid moves.
-//  */
-// void getValidMoves(GameModel& model, Moves& validMoves);
-
-// /**
-//  * @brief Plays a move.
-//  *
-//  * @param model The game model.
-//  * @param square The move.
-//  * @return Move accepted.
-//  */
-// bool playMove(GameModel& model, int8_t move);
-
-// #endif
-
 /**
  * @brief Implements the Reversi game model
  * @author Marc S. Ressl
@@ -492,5 +315,195 @@ BoardState_t makeMove(Board_t& board, PlayerColor_t& currentPlayer, Move_t move)
  * @param state The saved state to restore
  */
 void unmakeMove(Board_t& board, PlayerColor_t& currentPlayer, const BoardState_t& state);
+
+/**
+ * @brief Gets move count without allocating vector (for mobility evaluation).
+ *
+ * @param board The board state
+ * @param player The player
+ * @return Number of valid moves
+ */
+int getMoveCount(const Board_t& board, PlayerColor_t player);
+
+/**
+ * @brief Gets the opponent of a player.
+ *
+ * @param player Current player
+ * @return Opponent player
+ */
+inline PlayerColor_t getOpponent(PlayerColor_t player) {
+    return (player == PLAYER_BLACK) ? PLAYER_WHITE : PLAYER_BLACK;
+}
+
+/**
+ * @brief Gets player's bitboard from board.
+ *
+ * @param board The board state
+ * @param player The player
+ * @return Player's bitboard
+ */
+inline uint64_t getPlayerBitboard(const Board_t& board, PlayerColor_t player) {
+    return (player == PLAYER_BLACK) ? board.black : board.white;
+}
+
+/**
+ * @brief Gets opponent's bitboard from board.
+ *
+ * @param board The board state
+ * @param player Current player
+ * @return Opponent's bitboard
+ */
+inline uint64_t getOpponentBitboard(const Board_t& board, PlayerColor_t player) {
+    return (player == PLAYER_BLACK) ? board.white : board.black;
+}
+
+/**
+ * @brief Gets empty squares bitboard.
+ *
+ * @param board The board state
+ * @return Empty squares bitboard
+ */
+inline uint64_t getEmptyBitboard(const Board_t& board) {
+    return ~(board.black | board.white);
+}
+
+/**
+ * @brief Counts total discs on board.
+ *
+ * @param board The board state
+ * @return Number of discs (0-64)
+ */
+inline int getDiscCount(const Board_t& board) {
+    return countBits(board.black | board.white);
+}
+
+/**
+ * @brief Counts empty squares on board.
+ *
+ * @param board The board state
+ * @return Number of empty squares (0-64)
+ */
+inline int getEmptyCount(const Board_t& board) {
+    return 64 - getDiscCount(board);
+}
+
+/**
+ * @brief Checks if a move is valid (without generating full move list).
+ *
+ * @param board The board state
+ * @param player The player
+ * @param move The move to check
+ * @return True if move is legal
+ */
+bool isMoveValid(const Board_t& board, PlayerColor_t player, Move_t move);
+
+// ============================================================================
+// NEW: Bitboard constants for special squares (useful for evaluation)
+// ============================================================================
+
+// Corner squares (most valuable in Reversi)
+#define CORNERS 0x8100000000000081ULL  // a1, h1, a8, h8
+
+// X-squares (dangerous squares next to corners)
+#define X_SQUARES 0x4200000000004200ULL  // b2, g2, b7, g7
+
+// C-squares (edge squares next to corners)
+#define C_SQUARES 0x0042000000004200ULL  // b1, g1, a2, h2, a7, h7, b8, g8 (simplified)
+
+// Edge squares (excluding corners)
+#define EDGES 0x7E8181818181817EULL
+
+// Inner squares (center 6x6)
+#define INNER 0x007E7E7E7E7E7E00ULL
+
+// Center 4 squares (d4, e4, d5, e5)
+#define CENTER_4 0x0000001818000000ULL
+
+// ============================================================================
+// NEW: Advanced bitboard utilities (for future evaluation)
+// ============================================================================
+
+/**
+ * @brief Counts discs in a specific region (using mask).
+ *
+ * @param board The board state
+ * @param player The player
+ * @param mask Region mask (e.g., CORNERS, EDGES)
+ * @return Number of player's discs in masked region
+ */
+inline int countRegion(const Board_t& board, PlayerColor_t player, uint64_t mask) {
+    uint64_t playerBB = getPlayerBitboard(board, player);
+    return countBits(playerBB & mask);
+}
+
+/**
+ * @brief Checks if a square is occupied by player.
+ *
+ * @param board The board state
+ * @param player The player
+ * @param move Square position
+ * @return True if player occupies the square
+ */
+inline bool hasDisc(const Board_t& board, PlayerColor_t player, Move_t move) {
+    uint64_t playerBB = getPlayerBitboard(board, player);
+    return (playerBB & (1ULL << move)) != 0;
+}
+
+/**
+ * @brief Checks if a square is empty.
+ *
+ * @param board The board state
+ * @param move Square position
+ * @return True if square is empty
+ */
+inline bool isEmpty(const Board_t& board, Move_t move) {
+    uint64_t occupied = board.black | board.white;
+    return (occupied & (1ULL << move)) == 0;
+}
+
+/**
+ * @brief Gets all corner discs for a player.
+ *
+ * @param board The board state
+ * @param player The player
+ * @return Number of corners held by player
+ */
+inline int getCornerCount(const Board_t& board, PlayerColor_t player) {
+    return countRegion(board, player, CORNERS);
+}
+
+// ============================================================================
+// NEW: Board copying and comparison (useful for transposition tables)
+// ============================================================================
+
+/**
+ * @brief Copies board state.
+ *
+ * @param dest Destination board
+ * @param src Source board
+ */
+inline void copyBoard(Board_t& dest, const Board_t& src) {
+    dest.black = src.black;
+    dest.white = src.white;
+}
+
+/**
+ * @brief Compares two boards for equality.
+ *
+ * @param a First board
+ * @param b Second board
+ * @return True if boards are identical
+ */
+inline bool boardsEqual(const Board_t& a, const Board_t& b) {
+    return (a.black == b.black) && (a.white == b.white);
+}
+
+/**
+ * @brief Prints board to console (for debugging).
+ *
+ * @param board The board state
+ * @param currentPlayer Current player (optional, for display)
+ */
+void printBoardDebug(const Board_t& board, PlayerColor_t currentPlayer = PLAYER_BLACK);
 
 #endif
